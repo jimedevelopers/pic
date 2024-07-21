fcce34f1ffc850428157f8c58bc1c89d92385b13
jimedevelopers <jimedevelopers@gmail.com>
2024-07-21 21:06:41 +0530

    added files

---
+++ hello_name/__init__.py
@@ -0,0 +1,21 @@
+import check50
+
+@check50.check()
+def exists():
+    """hello_name.py exists"""
+    check50.exists("hello_name.py")
+
+
+@check50.check(exists)
+def test1():
+    """input of Jameel yields output of Hello Jameel"""
+    output = check50.run("python3 hello_name.py").stdin("Jameel", prompt=False).stdout("[Hh]ello Jameel")
+
+
+@check50.check(exists)
+def test14():
+    """input of Jime yields output of Hello Jime"""
+    output = check50.run("python3 hello_name.py").stdin("Jime", prompt=False).stdout("[Hh]ello Jime")
+
+
+
